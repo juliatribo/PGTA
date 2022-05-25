bc71ba912dd1327265f4de50b0b8a748d18aeff4
lauranuez <lauranuezzaragoza@gmail.com>
2022-05-25 02:38:40 +0200

    añadir tabla mapa

--- PGTA/Map.Designer.cs
+++ PGTA/Map.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
             this.button1 = new System.Windows.Forms.Button();
@@ -43,7 +46,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // gMapControl1
@@ -55,7 +60,7 @@
             this.gMapControl1.GrayScaleMode = false;
             this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gMapControl1.LevelsKeepInMemmory = 5;
-            this.gMapControl1.Location = new System.Drawing.Point(31, 49);
+            this.gMapControl1.Location = new System.Drawing.Point(31, 143);
             this.gMapControl1.Margin = new System.Windows.Forms.Padding(2);
             this.gMapControl1.MarkersEnabled = true;
             this.gMapControl1.MaxZoom = 2;
@@ -70,7 +75,7 @@
             this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.gMapControl1.ShowTileGridLines = false;
-            this.gMapControl1.Size = new System.Drawing.Size(1344, 613);
+            this.gMapControl1.Size = new System.Drawing.Size(1344, 589);
             this.gMapControl1.TabIndex = 0;
             this.gMapControl1.Zoom = 0D;
             this.gMapControl1.OnMarkerClick += new GMap.NET.WindowsForms.MarkerClick(this.gMapControl1_OnMarkerClick);
@@ -208,12 +213,50 @@
             this.button8.UseVisualStyleBackColor = true;
             this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(1344, 90);
+            this.dataGridView1.TabIndex = 18;
+            // 
             // Map
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(1419, 699);
+            this.ClientSize = new System.Drawing.Size(1419, 743);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.numericUpDown2);
             this.Controls.Add(this.label3);
@@ -232,6 +275,7 @@
             this.Load += new System.EventHandler(this.Map_Load);
             this.SizeChanged += new System.EventHandler(this.Map_SizeChanged);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -253,5 +297,6 @@
         private Label label1;
         private Button button7;
         private Button button8;
+        private DataGridView dataGridView1;
     }
 }
--- PGTA/Map.cs
+++ PGTA/Map.cs
@@ -34,6 +34,8 @@ namespace PGTA
         double initialTime;
         System.TimeSpan timeSpan;
         int borrar = 0;
+        List<string> row = new List<string>();
+        DataTable dt = new DataTable();
 
         public Map()
         {
@@ -82,6 +84,31 @@ namespace PGTA
 
             label3.Text = "Target ID or Mode3A";
             Load_Small_Data();
+
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+            dataGridView1.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            dt.Columns.Add("Sic");
+            dt.Columns.Add("Sac");
+            dt.Columns.Add("Service ID");
+            dt.Columns.Add("Time Track Info");
+            dt.Columns.Add("Latitude (WGS-84)");
+            dt.Columns.Add("Longitude (WGS-84)");
+            dt.Columns.Add("X (Cartesian)");
+            dt.Columns.Add("Y (Cartesian)");
+            dt.Columns.Add("Vx (Cartesian)");
+            dt.Columns.Add("Vy (Cartesian)");
+            dt.Columns.Add("ax (Cartesian)");
+            dt.Columns.Add("ay (Cartesian)");
+            dt.Columns.Add("Octal Mode 3A");
+            dt.Columns.Add("Target Identification Status");
+            dt.Columns.Add("Target ID");
+            dt.Columns.Add("Track number");
+            dt.Columns.Add("Measured Flight Level");
+            dt.Columns.Add("Geometric Altitude (ft)");
+            dt.Columns.Add("Barometric Altitude (Click to expand)");
+            dt.Columns.Add("Rate of Climb/Descent (feet/minute)");
+            dt.Columns.Add("Track Mode 2 Code");
+            dataGridView1.DataSource = dt;
 
         }
         //Metodo para mover las aeronaves con cada tick del reloj
@@ -243,13 +270,14 @@ namespace PGTA
         {
             try
             {
+                dt.Clear();
                 timer1.Stop();
                 Bitmap plane1 = (Bitmap)Image.FromFile("img/plane2.png");
                 Bitmap plane = new Bitmap(plane1, new Size(plane1.Width / 12, plane1.Height / 12));
-
-
+                bool encontrado = false;
                 for (int i = 0; i < small_data.Length; i++)
                 {
+
                     if (small_data[small_counter].Target_id_245 != null)
                     {
                         if (small_data[i] != null && small_data[i].Target_id_245 == item.Tag.ToString())
@@ -257,19 +285,131 @@ namespace PGTA
                             GMarkerGoogle marker = new GMarkerGoogle(new PointLatLng(small_data[i].Latitude, small_data[i].Longitude), RotateImg(plane, small_data[i].Mag_heading));
                             marker.Tag = small_data[i].Target_id_245;
                             markerOverlay2.Markers.Add(marker);
-                            label3.Text = small_data[i].Target_id_245.ToString();
-
                         }
                     }
-                    else if (small_data[i] != null && small_data[i].Octal_mode3A == Convert.ToInt32(item.Tag))
+                    else if (small_data[i] != null && small_data[i].Octal_mode3A.ToString() == item.Tag.ToString())
                     {
                         GMarkerGoogle marker = new GMarkerGoogle(new PointLatLng(small_data[i].Latitude, small_data[i].Longitude), RotateImg(plane, small_data[i].Mag_heading));
                         marker.Tag = small_data[i].Octal_mode3A;
                         markerOverlay2.Markers.Add(marker);
-                        label3.Text = small_data[i].Octal_mode3A.ToString();
                     }
                 }
+                
+                for (int p = 0; p<small_data.Length && !encontrado; p++)
+                {
+                    if (item.Position.Lat == small_data[p].Latitude && item.Position.Lng == small_data[p].Longitude)
+                    {
+                        encontrado = true;
+                        //Rellenar tabla
+                        row.Add(small_data[p].Sic.ToString());
+                        row.Add(small_data[p].Sac.ToString()); ;
+                        row.Add(small_data[p].Service_Id.ToString());
+                        int hor, min, seg;
+                        hor = Convert.ToInt32(small_data[p].Time_track_info / 3600);
+                        min = Convert.ToInt32((small_data[p].Time_track_info - hor * 3600) / 60);
+                        seg = Convert.ToInt32(small_data[p].Time_track_info - (hor * 3600 + min * 60));
+                        string shor, smin, sseg;
+                        if (hor <= 9)
+                        {
+                            shor = "0" + hor.ToString();
+                        }
+                        else
+                        {
+                            shor = hor.ToString();
+                        }
+                        if (min <= 9)
+                        {
+                            smin = "0" + min.ToString();
+                        }
+                        else
+                        {
+                            smin = min.ToString();
+                        }
+                        if (seg <= 9)
+                        {
+                            sseg = "0" + seg.ToString();
+                        }
+                        else
+                        {
+                            sseg = seg.ToString();
+                        }
+                        string time = shor + ":" + smin + ":" + sseg;
+                        row.Add(time);
+                        double lat_red = Math.Round(small_data[p].Latitude, 2);
+                        row.Add(lat_red.ToString() + " º");
+                        double long_red = Math.Round(small_data[p].Longitude, 2);
+                        row.Add(long_red.ToString() + " º");
+                        row.Add(small_data[p].X.ToString() + " m");
+                        row.Add(small_data[p].Y.ToString() + " m");
+                        row.Add(small_data[p].Vx.ToString() + " m/s");
+                        row.Add(small_data[p].Vy.ToString() + " m/s");
+                        if (small_data[p].Ax != 0)
+                        {
+                            row.Add(small_data[p].Ax.ToString());
+                        }
+                        else
+                        {
+                            row.Add("-");
+                        }
+                        if (small_data[p].Ay != 0)
+                        {
+                            row.Add(small_data[p].Ay.ToString());
+                        }
+                        else
+                        {
+                            row.Add("-");
+                        }
+                        row.Add(small_data[p].Octal_mode3A.ToString());
+                        if (small_data[p].Target_id_status != null)
+                        {
+                            row.Add(small_data[p].Target_id_status.ToString());
+                        }
+                        else
+                        {
+                            row.Add("-");
+                        }
+                        if (small_data[p].Target_id_245 != null)
+                        {
+                            row.Add(small_data[p].Target_id_245.ToString());
+                        }
+                        else
+                        {
+                            row.Add("-");
+                        }
+                        row.Add(small_data[p].Track_num.ToString());
+                        row.Add(small_data[p].FL.ToString());
+                        row.Add(small_data[p].TGA.ToString());
+                        string barometric_alt;
+                        if (small_data[p].CorrectionQNH)
+                        {
+                            barometric_alt = "QNH correction" + "\n" + "BA = " + small_data[p].TBA.ToString() + " ft";
+                        }
+                        else
+                        {
+                            barometric_alt = "No QNH correction" + "\n" + "BA = " + small_data[p].TBA.ToString() + " ft";
+                        }
+                        row.Add(barometric_alt);
+                        row.Add(small_data[p].Rate.ToString());
+                        string Octal_mode2A = "";
+                        if (small_data[p].Octal_mode2A == 0)
+                        {
+                            Octal_mode2A = "-";
+                        }
+                        else
+                        {
+                            Octal_mode2A = small_data[p].Octal_mode2A.ToString();
+                        }
+                        row.Add(Octal_mode2A);
+                    }
+                }
+                
                 gMapControl1.Overlays.Add(markerOverlay2);
+                DataRow row2 = dt.NewRow();
+                row2.ItemArray = row.ToArray();
+                dt.Rows.Add(row2);
+                dataGridView1.DataSource = dt;
+                row.Clear();
+
                 timer1.Start();
             }
             catch (Exception ex) { MessageBox.Show(ex.ToString()); }
@@ -290,10 +430,11 @@ namespace PGTA
             }
         }
 
-        //Boton para eliminar las trazas
+        //Boton para eliminar las trazas y la tabla
         private void button5_Click(object sender, EventArgs e)
         {
             markerOverlay2.Markers.Clear();
+            dt.Clear();
         }
 
         //Boton para exportar a kml las posiciones de los aviones
